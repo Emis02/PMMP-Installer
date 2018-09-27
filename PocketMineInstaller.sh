de09987cccf959e2d80b63827786f88e85b55c66
TheNewEmis <alessiomorning1@gmail.com>
2018-09-27 07:42:02 +0200

    New update 3.2.3

--- PocketMineInstaller.sh
+++ PocketMineInstaller.sh
@@ -7,7 +7,7 @@ apt get install untar
 git clone https://github.com/Emis02/StartPmmp.git
 cd StartPmmp
 wget https://jenkins.pmmp.io/job/PHP-7.2-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz
-wget https://jenkins.pmmp.io/job/PocketMine-MP/1386/artifact/PocketMine-MP.phar
+wget https://jenkins.pmmp.io/job/PocketMine-MP/1434//artifact/PocketMine-MP.phar
 tar -xvzf PHP_Linux-x86_64.tar.gz
 rm -r PHP_Linux-x86_64.tar.gz
 echo hai installato correttamente PMMP By Emis
